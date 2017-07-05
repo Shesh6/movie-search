b9f80710a0a82b9f6c933a0982db7c3cec28ee57
Shesh6 <rbananatwister@gmail.com>
2017-07-05 15:42:29 +0000

    add posters

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 var express = require("express");
 var app = express();
 var request = require("request");
+var $ = require('jquery');
 // var bs = require("bootstrap");
 
 app.use(express.static("public"));
